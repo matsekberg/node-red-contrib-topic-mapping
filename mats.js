d97a87c2bad44918e9e5b9f3fd26514640c76ce1
kkmeg <konsult.mats.ekberg@tullverket.se>
2017-10-19 16:26:36 +0200

    1st commit

---
+++ mats.js
@@ -0,0 +1,37 @@
+module.exports = function(RED) {
+
+    var mqttregex = require('mqtt-regex');
+
+    function MatsNodeIn(config) {
+        RED.nodes.createNode(this,config);
+        this.pattern = config.pattern;
+        this.mapfilename = config.mapfile;
+        var node = this;
+        var mapping = require(this.mapfilename);
+
+        node.on('input', function(msg) {
+            this.status({fill:"red",shape:"ring",text:msg.topic + " processed"});
+
+            var room_message_info = mqttregex(this.pattern).exec;
+            var topic = msg.topic;
+            var message_content = msg.payload;
+            var params = room_message_info(topic);
+            msg.items = params;
+            msg.mapping = mapping;
+            node.send(msg);
+        });
+    }
+    RED.nodes.registerType("mats in",MatsNodeIn);
+
+    function MatsNodeOut(config) {
+        RED.nodes.createNode(this,config);
+        this.test = config.test;
+        var node = this;
+        node.on('input', function(msg) {
+            msg.payload = msg.payload.toLowerCase() + this.test;
+            this.status({fill:"green",shape:"ring",text:this.test + " added"});
+            node.send(msg);
+        });
+    }
+    RED.nodes.registerType("mats out",MatsNodeOut);
+}
